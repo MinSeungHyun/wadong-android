69ae2cdcb49e924b65dfa5e8ff2db14adbed9e91
SeungHyunMin <asdf014563@naver.com>
2020-01-18 23:56:48 +0900

    Create AddStoryActivity

---
+++ app/src/main/java/com/seunghyun/wadong/ui/add_story/AddStoryActivity.kt
@@ -0,0 +1,10 @@
+package com.seunghyun.wadong.ui.add_story
+
+import com.seunghyun.wadong.R
+import com.seunghyun.wadong.databinding.ActivityAddStoryBinding
+import com.seunghyun.wadong.ui.BindingActivity
+
+class AddStoryActivity : BindingActivity<ActivityAddStoryBinding>() {
+    override fun getLayoutId() = R.layout.activity_add_story
+
+}
