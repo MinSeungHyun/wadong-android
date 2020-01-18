72f757019c157fef2cd84e2950823349a90ce07f
SeungHyunMin <asdf014563@naver.com>
2020-01-18 21:17:58 +0900

    Move code

--- app/src/main/java/com/seunghyun/wadong/MainViewModel.kt
+++ app/src/main/java/com/seunghyun/wadong/MainViewModel.kt
@@ -1,11 +1,28 @@
 package com.seunghyun.wadong
 
 import androidx.databinding.ObservableField
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainViewModel {
     val selectedCategory = ObservableField<Category>(Category.ALL)
 
+    private lateinit var map: GoogleMap
+
     fun onCategoryButtonClick(clickedCategory: Category) {
         selectedCategory.set(clickedCategory)
+    }
+
+    fun onMapReady(map: GoogleMap) {
+        this.map = map
+        val currentLocation = LatLng(37.533623, 126.960609)
+
+        map.apply {
+            addMarker(MarkerOptions().position(currentLocation).title("현위치"))
+            moveCamera(CameraUpdateFactory.newLatLng(currentLocation))
+            moveCamera(CameraUpdateFactory.zoomTo(15f))
+        }
     }
 }
--- app/src/main/java/com/seunghyun/wadong/MapsActivity.kt
+++ app/src/main/java/com/seunghyun/wadong/MapsActivity.kt
@@ -1,38 +1,21 @@
 package com.seunghyun.wadong
 
 import android.os.Bundle
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.seunghyun.wadong.databinding.ActivityMapsBinding
 
-class MapsActivity : BindingActivity<ActivityMapsBinding>(), OnMapReadyCallback {
-    private lateinit var map: GoogleMap
+class MapsActivity : BindingActivity<ActivityMapsBinding>() {
     private val viewModel by lazy { MainViewModel() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         with(supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment) {
-            getMapAsync(this@MapsActivity)
+            getMapAsync(viewModel::onMapReady)
         }
 
         binding.apply {
             vm = viewModel
-        }
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
-        val currentLocation = LatLng(37.533623, 126.960609)
-
-        map.apply {
-            addMarker(MarkerOptions().position(currentLocation).title("현위치"))
-            moveCamera(CameraUpdateFactory.newLatLng(currentLocation))
-            moveCamera(CameraUpdateFactory.zoomTo(15f))
         }
     }
 
