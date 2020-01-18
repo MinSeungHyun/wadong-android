96a8defd1042eb3ee0472bd5a854fd3cb4cffba5
SeungHyunMin <asdf014563@naver.com>
2020-01-18 17:02:59 +0900

    Change default location and camera

--- app/src/main/java/com/seunghyun/wadong/MapsActivity.kt
+++ app/src/main/java/com/seunghyun/wadong/MapsActivity.kt
@@ -10,19 +10,24 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-    private lateinit var mMap: GoogleMap
+    private lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        with(supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment) {
+            getMapAsync(this@MapsActivity)
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map = googleMap
+        val currentLocation = LatLng(37.533623, 126.960609)
+
+        map.apply {
+            addMarker(MarkerOptions().position(currentLocation).title("현위치"))
+            moveCamera(CameraUpdateFactory.newLatLng(currentLocation))
+            moveCamera(CameraUpdateFactory.zoomTo(15f))
+        }
     }
 }
